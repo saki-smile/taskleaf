330c5c5d6d57e41df8e37e7f886be81fc2545343
saki-smile <sa.su.ko0419@gmail.com>
2021-03-21 16:20:45 +0000

    fix user-admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,8 +14,8 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#(@user.name)」を登録しました。"
+    if @user.save!
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
@@ -29,7 +29,7 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to admin_user_url(@user), notice: "ユーザー「#(@user.name)」を更新しました。"
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :edit
     end
@@ -38,7 +38,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to admin_users_url, notice: "ユーザー「#(@user.name)」を削除しました。"
+    redirect_to admin_users_url, notice: "ユーザー「#{user.name}」を削除しました。"
   end
 
   private
