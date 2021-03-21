386372645f1bfb36dc0ce391304599ea2494e0e8
saki-smile <sa.su.ko0419@gmail.com>
2021-03-21 07:28:07 +0000

    add task validates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク「#{task.name}」を登録しました。"
+    else
+      render :new
+    end
+    # redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
     # 上記は、下のように二行で書くのと同じ意味
     # flash[:notice] = "タスク「#{task.name}」を登録しました。"
     # redirect_to tasks_url
